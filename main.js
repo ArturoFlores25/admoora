c3859c733872cbb5ecbf43863042b044b9bb3a9c
Arturo Flores <arturoflores25@outlook.com>
2022-07-25 01:08:05 -0500

    Inicio

---
+++ main.js
@@ -0,0 +1,27 @@
+alert("¿La Admoora se baña?🤔")
+
+    var bañoAdmoora = prompt ("Responde Sí o No🤓");
+
+    switch (bañoAdmoora){
+        case "Sí":
+            document.write("<h1>Mentira porque es 😤</h1>");
+            break;
+        case "Si":
+            document.write("<h1>Mentira porque es otaku😤</h1>");
+            break;
+        case "si":
+            document.write("<h1>Mentira porque es otaku😤</h1>");
+            break;
+        case "sí":
+            document.write("<h1>Mentira porque es otaku😤</h1>");
+            break;
+        case "No":
+            document.write("<h1>Pos no, vieja mugres😩</h1>");
+            break;
+        case "no":
+            document.write("<h1>Pos no, vieja mugres😩</h1>");
+            break;
+    default:
+            document.write("<h1>Contesta la pregunta, qlo. 😡</h1>")
+
+    }
